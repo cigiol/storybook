47fef79c90a7b206d4f31f43a4284157783b5ede
Cihan Gürtürk <cihangurturk@Cihans-MBP.home>
2023-08-09 21:57:11 +0300

    init tailwind && storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,25 @@
+// import type { Preview } from "@storybook/react";
+
+// const preview: Preview = {
+//   parameters: {
+//     actions: { argTypesRegex: "^on[A-Z].*" },
+//     controls: {
+//       matchers: {
+//         color: /(background|color)$/i,
+//         date: /Date$/,
+//       },
+//     },
+//   },
+// };
+
+// export default preview;
+import "tailwindcss/tailwind.css";
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
